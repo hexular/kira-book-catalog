91d06b51957880052511778c4c9de523b06ec100
Dan <hexular@gmail.com>
2020-02-16 18:40:40 -0500

    add django rest and url pathing for book database

--- challenge/books/models.py
+++ challenge/books/models.py
@@ -4,5 +4,5 @@ from django.db import models
 
 class Book(models.Model):
   title = models.CharField(max_length=100)
-  autor = models.CharField(max_length=50)
+  author = models.CharField(max_length=50)
   quantity = models.IntegerField()
--- challenge/books/views.py
+++ challenge/books/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from .models import Book
+from .serializers import BookSerializer
+from rest_framework import generics
+
+class BookListCreate(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
