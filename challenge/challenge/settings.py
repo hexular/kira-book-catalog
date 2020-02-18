0918104fdb0f5f313cc00538a84c137282a56b73
Dan <hexular@gmail.com>
2020-02-17 21:18:40 -0500

    add settings for django rest api display

--- challenge/challenge/settings.py
+++ challenge/challenge/settings.py
@@ -47,9 +47,10 @@ INSTALLED_APPS = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_RENDERER_CLASSES': (
+    'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
-    )
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ]
 }
 
 MIDDLEWARE = [
