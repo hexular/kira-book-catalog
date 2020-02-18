4de48ad0a429f9b53e8714471b3073d21bd6dade
Dan <hexular@gmail.com>
2020-02-18 11:45:58 -0500

    add reserved quantity to database

--- challenge/books/models.py
+++ challenge/books/models.py
@@ -6,3 +6,8 @@ class Book(models.Model):
   title = models.CharField(max_length=100)
   author = models.CharField(max_length=50)
   quantity = models.IntegerField()
+  reserved = models.IntegerField(default=0)
+
+# Added reserved to database in order for multiple concurrent users to be able to see changes in reservations.
+# The alternative would have been to store reservation data locally in React, which would not allow multiple users
+# to see changes in reservations, only their own.
--- challenge/books/serializers.py
+++ challenge/books/serializers.py
@@ -4,4 +4,4 @@ from .models import Book
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
-        fields = ('id', 'title', 'author', 'quantity')
+        fields = ('id', 'title', 'author', 'quantity', 'reserved')
