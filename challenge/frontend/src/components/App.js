9fbe24d985a8d602e9910d0bbc4c4bfbc86feeda
Dan <hexular@gmail.com>
2020-02-19 01:55:04 -0500

    add comments

--- challenge/frontend/src/components/App.js
+++ challenge/frontend/src/components/App.js
@@ -2,6 +2,14 @@ import React, { useState, useEffect } from "react";
 import { render } from "react-dom";
 import Book from './Book';
 
+// TODO: 
+// - Search by title
+// - Reserved by add name and click to expand by who
+//    - Render return button next to each individual reservation
+// - View by reserved or available
+// - Add button to send all API calls together at end
+//    - Implement caching for this
+
 export default function App() {
 
   const [data, setData] = useState([])
--- challenge/frontend/src/components/Book.js
+++ challenge/frontend/src/components/Book.js
@@ -1,17 +1,13 @@
 import React, { useState, useEffect } from "react";
 
 export default function Book(props) {
-  const disabled = true;
+
   const { id, title, author, quantity, reserved } = props.book;
 
-  console.log(props)
-  console.log('hello world')
-
   // PATCH method proved to be unnecessarily complicated given the Django REST framework
-  // Decided to use PUT with a partial update
+  // Decided to use PUT with a partial update to update the reserved part
   const reserveBook = change => {
     const newReserved = reserved + change;
-    console.log(reserved, change)
     fetch(`api/book/update/${id}`, {
       method: 'PUT',
       headers: {
